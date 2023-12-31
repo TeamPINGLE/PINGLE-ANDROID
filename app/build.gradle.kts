f3751c4d68855b79e15ec820afb3512764283de4
Do-Re-Min-Woo <kimy5878@naver.com>
2024-01-01 01:59:57 +0900

    [chore] #3minSdk 24 -> 28 변경
    lineHeight 속성이 api28부터 지원함.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         applicationId = "org.sopt.pingle"
-        minSdk = 24
+        minSdk = 28
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"

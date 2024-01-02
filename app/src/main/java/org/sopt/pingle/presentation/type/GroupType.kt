7be44af5166b5dd7357818f6d2867d01df9512f3
Do-Re-Min-Woo <kimy5878@naver.com>
2024-01-03 03:19:56 +0900

    [feat] #13 GroupType 및 BindingAdpater

---
+++ app/src/main/java/org/sopt/pingle/presentation/type/GroupType.kt
@@ -0,0 +1,19 @@
+package org.sopt.pingle.presentation.type
+
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+import org.sopt.pingle.R
+
+enum class GroupType(
+    @StringRes val groupTextRes: Int,
+    @DrawableRes val groupImageRes: Int,
+) {
+    ORIGINAL(
+        R.string.on_bording_original_group,
+        R.drawable.ic_launcher_background
+    ),
+    NEW(
+        R.string.on_bording_new_group,
+        R.drawable.ic_launcher_background
+    )
+}

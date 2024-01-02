a25773da299d6b2cbb38595df61110c5f2327ab6
Do-Re-Min-Woo <kimy5878@naver.com>
2024-01-02 14:32:38 +0900

    [chore] #13 ktlint 적용

--- app/src/main/java/org/sopt/pingle/presentation/ui/onbording/OnBordingActivity.kt
+++ app/src/main/java/org/sopt/pingle/presentation/ui/onbording/OnBordingActivity.kt
@@ -8,6 +8,5 @@ import org.sopt.pingle.util.base.BindingActivity
 class OnBordingActivity : BindingActivity<ActivityOnBordingBinding>(R.layout.activity_on_bording) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
     }
 }

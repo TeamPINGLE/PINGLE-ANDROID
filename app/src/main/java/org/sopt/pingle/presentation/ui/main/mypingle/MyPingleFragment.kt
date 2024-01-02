fa110d9659c3ef9a1ddf52fa6ce93ba3266d4d2f
jihyunniiii <jita1125@naver.com>
2024-01-02 19:43:08 +0900

    [feat] #16 bottom navigation에 fragment 및 Activity 연결

---
+++ app/src/main/java/org/sopt/pingle/presentation/ui/main/mypingle/MyPingleFragment.kt
@@ -0,0 +1,13 @@
+package org.sopt.pingle.presentation.ui.main.mypingle
+
+import android.os.Bundle
+import android.view.View
+import org.sopt.pingle.R
+import org.sopt.pingle.databinding.FragmentMyPingleBinding
+import org.sopt.pingle.util.base.BindingFragment
+
+class MyPingleFragment : BindingFragment<FragmentMyPingleBinding>(R.layout.fragment_my_pingle) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}

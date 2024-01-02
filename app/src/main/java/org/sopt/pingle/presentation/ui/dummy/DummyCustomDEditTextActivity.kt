2aa973b87f731d065d2a42365dce255c5c0f9c05
Dani43 <eldms0406@naver.com>
2024-01-02 23:07:27 +0900

    [chore] #6 ktlint 적용

--- app/src/main/java/org/sopt/pingle/presentation/ui/dummy/DummyCustomDEditTextActivity.kt
+++ app/src/main/java/org/sopt/pingle/presentation/ui/dummy/DummyCustomDEditTextActivity.kt
@@ -5,7 +5,7 @@ import org.sopt.pingle.R
 import org.sopt.pingle.databinding.ActivityEditTextCustomTestBinding
 import org.sopt.pingle.util.base.BindingActivity
 
-class DummyCustomDEditTextActivity: BindingActivity<ActivityEditTextCustomTestBinding>(R.layout.activity_edit_text_custom_test) {
+class DummyCustomDEditTextActivity : BindingActivity<ActivityEditTextCustomTestBinding>(R.layout.activity_edit_text_custom_test) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }

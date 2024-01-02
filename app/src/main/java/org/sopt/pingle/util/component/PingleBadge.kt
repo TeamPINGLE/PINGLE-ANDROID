bedf489a51699a8316f04d7c4876c90763007d36
jihyunniiii <jita1125@naver.com>
2024-01-03 03:05:43 +0900

    [chore] #19 PingleBadge attrs 제거

--- app/src/main/java/org/sopt/pingle/util/component/PingleBadge.kt
+++ app/src/main/java/org/sopt/pingle/util/component/PingleBadge.kt
@@ -3,11 +3,9 @@ package org.sopt.pingle.util.component
 import android.annotation.SuppressLint
 import android.content.Context
 import android.content.res.ColorStateList
-import android.content.res.TypedArray
 import android.util.AttributeSet
 import android.view.LayoutInflater
 import androidx.constraintlayout.widget.ConstraintLayout
-import org.sopt.pingle.R
 import org.sopt.pingle.databinding.BadgePingleBinding
 import org.sopt.pingle.presentation.type.CategoryType
 
@@ -22,27 +20,6 @@ class PingleBadge @JvmOverloads constructor(
     init {
         val inflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
         binding = BadgePingleBinding.inflate(inflater, this, true)
-
-        val typedArray =
-            context.obtainStyledAttributes(attrs, R.styleable.pingleBadge, defStyleAttr, DEF)
-
-        initView(typedArray)
-        typedArray.recycle()
-    }
-
-    private fun initView(typedArray: TypedArray) {
-        typedArray.apply {
-            with(binding.tvBadgePingleText) {
-                text = typedArray.getText(R.styleable.pingleBadge_pingleBadgeCategoryName)
-                setTextColor(typedArray.getColor(R.styleable.pingleBadge_pingleBadgeTextColor, DEF))
-                backgroundTintList = ColorStateList.valueOf(
-                    typedArray.getColor(
-                        R.styleable.pingleBadge_pingleBadgeBackgroundColor,
-                        DEF
-                    )
-                )
-            }
-        }
     }
 
     fun setBadgeCategoryType(categoryType: CategoryType) {
@@ -52,9 +29,5 @@ class PingleBadge @JvmOverloads constructor(
             backgroundTintList =
                 ColorStateList.valueOf(context.getColor(categoryType.backgroundBadgeColor))
         }
-    }
-
-    companion object {
-        const val DEF = 0
     }
 }

7bb88afd778417e739e096fc6e71e337ee357985
Do-Re-Min-Woo <kimy5878@naver.com>
2024-01-02 14:27:38 +0900

    [feat] #13 OnBardingActivity 뷰 구현

---
+++ app/src/main/java/org/sopt/pingle/presentation/ui/onbording/OnBordingActivity.kt
@@ -0,0 +1,13 @@
+package org.sopt.pingle.presentation.ui.onbording
+
+import android.os.Bundle
+import org.sopt.pingle.R
+import org.sopt.pingle.databinding.ActivityOnBordingBinding
+import org.sopt.pingle.util.base.BindingActivity
+
+class OnBordingActivity : BindingActivity<ActivityOnBordingBinding>(R.layout.activity_on_bording) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}

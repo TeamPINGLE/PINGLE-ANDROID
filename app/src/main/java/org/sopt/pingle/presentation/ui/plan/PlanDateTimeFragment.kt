7c83a3827deb3ffa5d9c69df63596419a58458bb
Dani43 <eldms0406@naver.com>
2024-01-03 20:53:26 +0900

    [feat] #21 필요한 프래그먼트 추가

---
+++ app/src/main/java/org/sopt/pingle/presentation/ui/plan/PlanDateTimeFragment.kt
@@ -0,0 +1,13 @@
+package org.sopt.pingle.presentation.ui.plan
+
+import android.os.Bundle
+import android.view.View
+import org.sopt.pingle.R
+import org.sopt.pingle.databinding.FragmentPlanDateTimeBinding
+import org.sopt.pingle.util.base.BindingFragment
+
+class PlanDateTimeFragment : BindingFragment<FragmentPlanDateTimeBinding>(R.layout.fragment_plan_date_time) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}

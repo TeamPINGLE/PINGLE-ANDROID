5cf163c86319d99891b18053feb7663b8c0a767f
Dani43 <eldms0406@naver.com>
2024-01-03 20:53:11 +0900

    [feat] #21 프래그먼트 위치 상태 관리를 위한 StateFlow 추가

---
+++ app/src/main/java/org/sopt/pingle/presentation/ui/plan/PlanViewModel.kt
@@ -0,0 +1,14 @@
+package org.sopt.pingle.presentation.ui.plan
+
+import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+
+class PlanViewModel : ViewModel() {
+    private val _currentPage = MutableStateFlow(1)
+    val currentPage get() = _currentPage.asStateFlow()
+
+    fun setCurrentPage(position: Int) {
+        _currentPage.value = position
+    }
+}

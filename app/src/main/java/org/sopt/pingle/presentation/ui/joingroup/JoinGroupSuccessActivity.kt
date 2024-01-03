db31cddb8b207d1b31dc680a9bd2171585b69b24
DoReMinWoo <kimy5878@naver.com>
2024-01-03 15:47:30 +0900

    [feat] TextView 2개로 나누기
    -> 폰트로 인해서 행간이 적용되지 않음.

--- app/src/main/java/org/sopt/pingle/presentation/ui/joingroup/JoinGroupSuccessActivity.kt
+++ app/src/main/java/org/sopt/pingle/presentation/ui/joingroup/JoinGroupSuccessActivity.kt
@@ -15,19 +15,41 @@ class JoinGroupSuccessActivity :
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        setDescriptionTextStyle()
+        initLayout()
     }
 
-    private fun setDescriptionTextStyle() {
-        val spannableText = SpannableString(binding.tvJoinGroupSuccessDescription.text)
+    private fun initLayout() {
+        setDescriptionTextStyle(countGroupName())
+    }
+
+    private fun getGroupName(): String {
+        // TODO 이전 화면에서 Intent를 통해서 groupName을 가져옴
+
+        return "SOPT"
+    }
+
+    private fun countGroupName() = getGroupName().length
+
+    private fun setDescription() {
+        binding.tvJoinGroupSuccessDescriptionGroupName.text =
+            getString(
+                R.string.join_group_success_description_group_name,
+                getGroupName()
+            )
+    }
+
+    private fun setDescriptionTextStyle(groupNameCount: Int) {
+        setDescription()
+
+        val spannableText = SpannableString(binding.tvJoinGroupSuccessDescriptionGroupName.text)
 
         spannableText.setSpan(
             TextAppearanceSpan(
                 this@JoinGroupSuccessActivity,
                 R.style.TextAppearance_Pingle_Sub_Semi_16
             ),
-            START,
-            END,
+            GROUP_NAME_START,
+            groupNameCount,
             Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
         )
 
@@ -38,15 +60,14 @@ class JoinGroupSuccessActivity :
                     R.color.g_01
                 )
             ),
-            START,
-            END,
+            GROUP_NAME_START,
+            groupNameCount,
             Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
         )
-        binding.tvJoinGroupSuccessDescription.text = spannableText
+        binding.tvJoinGroupSuccessDescriptionGroupName.text = spannableText
     }
 
     companion object {
-        const val START = 0
-        const val END = 3
+        const val GROUP_NAME_START = 0
     }
 }

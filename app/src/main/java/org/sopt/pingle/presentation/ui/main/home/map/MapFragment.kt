f09dbc166139914c3c5e61111910f1c0fec9787d
jihyunniiii <jita1125@naver.com>
2024-01-03 16:58:10 +0900

    [chore] #25 mapFragment 설정 로직 수정

--- app/src/main/java/org/sopt/pingle/presentation/ui/main/home/map/MapFragment.kt
+++ app/src/main/java/org/sopt/pingle/presentation/ui/main/home/map/MapFragment.kt
@@ -1,14 +1,20 @@
 package org.sopt.pingle.presentation.ui.main.home.map
 
+import android.Manifest
 import android.os.Bundle
 import android.view.View
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.fragment.app.add
+import androidx.fragment.app.commit
 import com.naver.maps.map.MapFragment
 import com.naver.maps.map.NaverMap
+import com.naver.maps.map.OnMapReadyCallback
 import org.sopt.pingle.R
 import org.sopt.pingle.databinding.FragmentMapBinding
 import org.sopt.pingle.util.base.BindingFragment
 
-class MapFragment : BindingFragment<FragmentMapBinding>(R.layout.fragment_map) {
+class MapFragment : BindingFragment<FragmentMapBinding>(R.layout.fragment_map), OnMapReadyCallback {
+    private lateinit var naverMap: NaverMap
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
@@ -18,16 +24,27 @@ class MapFragment : BindingFragment<FragmentMapBinding>(R.layout.fragment_map) {
     private fun initMapFragment() {
         val mapFragment =
             childFragmentManager.findFragmentById(R.id.fragment_map_naver_map) as MapFragment?
-        mapFragment?.getMapAsync { naverMap ->
-            with(naverMap) {
-                isNightModeEnabled = true
-                mapType = NaverMap.MapType.Navi
-            }
+                ?: MapFragment.newInstance().also {
+                    childFragmentManager.commit {
+                        add<MapFragment>(R.id.fragment_map_naver_map)
+                        setReorderingAllowed(true)
+                    }
+                }
 
-            with(naverMap.uiSettings) {
-                isZoomControlEnabled = false
-                isScaleBarEnabled = false
-            }
+        mapFragment.getMapAsync(this)
+    }
+
+    override fun onMapReady(naverMap: NaverMap) {
+        this.naverMap = naverMap
+
+        with(naverMap) {
+            isNightModeEnabled = true
+            mapType = NaverMap.MapType.Navi
+        }
+
+        with(naverMap.uiSettings) {
+            isZoomControlEnabled = false
+            isScaleBarEnabled = false
         }
     }
 }

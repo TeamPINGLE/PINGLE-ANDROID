0373791c1617258d4e510ae43d2a85603f6616bc
DoReMinWoo <kimy5878@naver.com>
2024-01-03 18:28:17 +0900

    [feat] #26 기존 단체 입장 뷰 구현

---
+++ app/src/main/java/org/sopt/pingle/presentation/ui/joingroup/JoinGroupCodeActivity.kt
@@ -0,0 +1,13 @@
+package org.sopt.pingle.presentation.ui.joingroup
+
+import android.os.Bundle
+import org.sopt.pingle.R
+import org.sopt.pingle.databinding.ActivityJoinGroupCodeBinding
+import org.sopt.pingle.util.base.BindingActivity
+
+class JoinGroupCodeActivity :
+    BindingActivity<ActivityJoinGroupCodeBinding>(R.layout.activity_join_group_code) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}

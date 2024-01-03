4c6a02c02a58b177b5616465c3dcac1d5178f31d
jihyunniiii <jita1125@naver.com>
2024-01-03 15:58:31 +0900

    [feat] #25 map fragment 구현 (지도 띄우기)

--- app/src/main/java/org/sopt/pingle/presentation/ui/main/home/map/MapFragment.kt
+++ app/src/main/java/org/sopt/pingle/presentation/ui/main/home/map/MapFragment.kt
@@ -2,6 +2,8 @@ package org.sopt.pingle.presentation.ui.main.home.map
 
 import android.os.Bundle
 import android.view.View
+import com.naver.maps.map.MapFragment
+import com.naver.maps.map.NaverMap
 import org.sopt.pingle.R
 import org.sopt.pingle.databinding.FragmentMapBinding
 import org.sopt.pingle.util.base.BindingFragment
@@ -9,5 +11,16 @@ import org.sopt.pingle.util.base.BindingFragment
 class MapFragment : BindingFragment<FragmentMapBinding>(R.layout.fragment_map) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        initMapFragment()
+    }
+
+    private fun initMapFragment() {
+        val mapFragment =
+            childFragmentManager.findFragmentById(R.id.fragment_map_naver_map) as MapFragment?
+        mapFragment?.getMapAsync {
+            it.isNightModeEnabled = true
+            it.mapType = NaverMap.MapType.Navi
+        }
     }
 }

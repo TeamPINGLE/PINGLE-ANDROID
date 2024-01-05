cc2372ebc3eea59fd11c6204175afb387c510a49
Do-Re-Min-Woo <kimy5878@naver.com>
2024-01-05 19:12:39 +0900

    [chore] #14 코드리뷰 반영

--- app/src/main/java/org/sopt/pingle/presentation/ui/joingroup/JoinGroupSuccessActivity.kt
+++ app/src/main/java/org/sopt/pingle/presentation/ui/joingroup/JoinGroupSuccessActivity.kt
@@ -12,6 +12,7 @@ import org.sopt.pingle.util.base.BindingActivity
 
 class JoinGroupSuccessActivity :
     BindingActivity<ActivityJoinGroupSuccessBinding>(R.layout.activity_join_group_success) {
+    private lateinit var groupName: String
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -19,52 +20,40 @@ class JoinGroupSuccessActivity :
     }
 
     private fun initLayout() {
-        setDescriptionTextStyle(countGroupName())
-    }
-
-    private fun getGroupName(): String {
         // TODO 이전 화면에서 Intent를 통해서 groupName을 가져옴
+        groupName = "SOPT"
 
-        return "SOPT"
-    }
-
-    private fun countGroupName() = getGroupName().length
-
-    private fun setDescription() {
-        binding.tvJoinGroupSuccessDescriptionGroupName.text =
-            getString(
-                R.string.join_group_success_description_group_name,
-                getGroupName()
-            )
+        setDescriptionTextStyle(groupName.length)
     }
 
     private fun setDescriptionTextStyle(groupNameCount: Int) {
-        setDescription()
-
-        val spannableText = SpannableString(binding.tvJoinGroupSuccessDescriptionGroupName.text)
-
-        spannableText.setSpan(
-            TextAppearanceSpan(
-                this@JoinGroupSuccessActivity,
-                R.style.TextAppearance_Pingle_Sub_Semi_16
-            ),
-            GROUP_NAME_START,
-            groupNameCount,
-            Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
-        )
-
-        spannableText.setSpan(
-            ForegroundColorSpan(
-                ContextCompat.getColor(
+        binding.tvJoinGroupSuccessDescriptionGroupName.text = SpannableString(
+            getString(
+                R.string.join_group_success_description_group_name,
+                groupName
+            )
+        ).apply {
+            setSpan(
+                TextAppearanceSpan(
                     this@JoinGroupSuccessActivity,
-                    R.color.g_01
-                )
-            ),
-            GROUP_NAME_START,
-            groupNameCount,
-            Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
-        )
-        binding.tvJoinGroupSuccessDescriptionGroupName.text = spannableText
+                    R.style.TextAppearance_Pingle_Sub_Semi_16
+                ),
+                GROUP_NAME_START,
+                groupNameCount,
+                Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+            )
+            setSpan(
+                ForegroundColorSpan(
+                    ContextCompat.getColor(
+                        this@JoinGroupSuccessActivity,
+                        R.color.g_01
+                    )
+                ),
+                GROUP_NAME_START,
+                groupNameCount,
+                Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+            )
+        }
     }
 
     companion object {

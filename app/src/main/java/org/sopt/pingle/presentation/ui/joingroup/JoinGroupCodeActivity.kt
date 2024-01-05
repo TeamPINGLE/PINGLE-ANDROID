c822f8ba45a5f5088fbef798281a4b389afde29f
Do-Re-Min-Woo <kimy5878@naver.com>
2024-01-05 18:42:15 +0900

    [feat] #26 더미데이터 확인

--- app/src/main/java/org/sopt/pingle/presentation/ui/joingroup/JoinGroupCodeActivity.kt
+++ app/src/main/java/org/sopt/pingle/presentation/ui/joingroup/JoinGroupCodeActivity.kt
@@ -1,13 +1,43 @@
 package org.sopt.pingle.presentation.ui.joingroup
 
 import android.os.Bundle
+import androidx.activity.viewModels
+import dagger.hilt.android.AndroidEntryPoint
 import org.sopt.pingle.R
 import org.sopt.pingle.databinding.ActivityJoinGroupCodeBinding
 import org.sopt.pingle.util.base.BindingActivity
+import org.sopt.pingle.util.component.CustomSnackbar
 
+@AndroidEntryPoint
 class JoinGroupCodeActivity :
     BindingActivity<ActivityJoinGroupCodeBinding>(R.layout.activity_join_group_code) {
+    private val joinViewModel by viewModels<JoinViewModel>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        initLayout()
+        addObservers()
+    }
+
+    private fun initLayout() {
+        // TODO Snackbar 확인용
+        binding.btnJoinGroupCodeNext.setOnClickListener {
+            CustomSnackbar.makeSnackbar(binding.root, "잘못된 코드입니다.", 97)
+        }
+    }
+
+    private fun addObservers() {
+        joinViewModel.run {
+            joinGroupData.observe(this@JoinGroupCodeActivity) {
+                binding.run {
+                    tvJoinGroupCodeGroupType.text = it.keyword
+                    tvJoinGroupCodeGroupName.text = it.name
+                    tvJoinGroupCodeMeetingCount.text =
+                        getString(R.string.join_group_code_meeting_count, it.meetingCount)
+                    tvJoinGroupCodeParticipantCount.text =
+                        getString(R.string.join_group_code_participant_count, it.participantCount)
+                }
+            }
+        }
     }
 }

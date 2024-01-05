96861da1b8e4a959541fd05bfdceaedff3a1dde6
Dani43 <eldms0406@naver.com>
2024-01-06 01:39:18 +0900

    [mod] #21 position값 초기값 수정 및 마진 값 수정

--- app/src/main/java/org/sopt/pingle/presentation/ui/main/plan/PlanViewModel.kt
+++ app/src/main/java/org/sopt/pingle/presentation/ui/main/plan/PlanViewModel.kt
@@ -5,10 +5,14 @@ import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 
 class PlanViewModel : ViewModel() {
-    private val _currentPage = MutableStateFlow(1)
+    private val _currentPage = MutableStateFlow(FIRST_PAGE_POSITION)
     val currentPage get() = _currentPage.asStateFlow()
 
     fun setCurrentPage(position: Int) {
         _currentPage.value = position
+    }
+
+    companion object {
+        const val FIRST_PAGE_POSITION = 0
     }
 }

8f14060fc858606f4e5536afc6773f8a5e0307f5
Do-Re-Min-Woo <kimy5878@naver.com>
2024-01-05 21:33:59 +0900

    [feat] #36 로그인 화면 뷰 작업 완료

---
+++ app/src/main/java/org/sopt/pingle/presentation/ui/auth/AuthActivity.kt
@@ -0,0 +1,12 @@
+package org.sopt.pingle.presentation.ui.auth
+
+import android.os.Bundle
+import org.sopt.pingle.R
+import org.sopt.pingle.databinding.ActivityAuthBinding
+import org.sopt.pingle.util.base.BindingActivity
+
+class AuthActivity : BindingActivity<ActivityAuthBinding>(R.layout.activity_auth) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}

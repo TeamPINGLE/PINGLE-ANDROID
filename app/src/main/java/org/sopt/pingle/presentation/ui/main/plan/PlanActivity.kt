7bdcadd96d952b0aeba34b9363c68d8d8a71f6f1
JIEUNI <jieundaeun@naver.com>
2024-01-06 06:26:38 +0900

    [chore] #42 PlanActivity fragmentList에 내용 요약/확인 프래그먼트 추가

--- app/src/main/java/org/sopt/pingle/presentation/ui/main/plan/PlanActivity.kt
+++ app/src/main/java/org/sopt/pingle/presentation/ui/main/plan/PlanActivity.kt
@@ -30,6 +30,7 @@ class PlanActivity : BindingActivity<ActivityPlanBinding>(R.layout.activity_plan
             add(PlanTitleFragment())
             add(PlanDateTimeFragment())
             add(PlanOpenChattingFragment())
+            add(PlanSummaryConfirmationFragment())
         }
 
         val adapter = PlanFragmentStateAdapter(fragmentList, this)
@@ -37,12 +38,12 @@ class PlanActivity : BindingActivity<ActivityPlanBinding>(R.layout.activity_plan
             this.adapter = adapter
             isUserInputEnabled = false
             registerOnPageChangeCallback(object :
-                    ViewPager2.OnPageChangeCallback() {
-                    override fun onPageSelected(position: Int) {
-                        super.onPageSelected(position)
-                        planViewModel.setCurrentPage(position)
-                    }
-                })
+                ViewPager2.OnPageChangeCallback() {
+                override fun onPageSelected(position: Int) {
+                    super.onPageSelected(position)
+                    planViewModel.setCurrentPage(position)
+                }
+            })
         }
     }
 

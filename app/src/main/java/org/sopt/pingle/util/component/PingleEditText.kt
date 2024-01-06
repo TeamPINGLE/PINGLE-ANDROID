14f65714b701fb16dcc3d67212dcb2cb2aaec69c
Dani43 <eldms0406@naver.com>
2024-01-07 01:23:43 +0900

    [mod] #47 커스텀 에딧 텍스트가 양방향 데이터 바인딩이 가능하도록 bindingAdapter, InverseBindingAdpater 구현

--- app/src/main/java/org/sopt/pingle/util/component/PingleEditText.kt
+++ app/src/main/java/org/sopt/pingle/util/component/PingleEditText.kt
@@ -6,6 +6,8 @@ import android.content.res.TypedArray
 import android.util.AttributeSet
 import android.view.LayoutInflater
 import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.databinding.DataBindingComponent
+import androidx.databinding.DataBindingUtil
 import org.sopt.pingle.R
 import org.sopt.pingle.databinding.EditTextPingleBinding
 
@@ -15,10 +17,17 @@ class PingleEditText @JvmOverloads constructor(
     attrs: AttributeSet? = null,
     defStyleAttr: Int = 0
 ) : ConstraintLayout(context, attrs, defStyleAttr) {
-    private lateinit var binding: EditTextPingleBinding
-    private var _etText: String? = null
+    private var binding: EditTextPingleBinding
+    val editText get() = binding.etEditText
 
     init {
+        binding = DataBindingUtil.inflate(
+            LayoutInflater.from(context),
+            R.layout.edit_text_pingle,
+            this,
+            true,
+        )
+        binding.view = this
         val typedArray = context.obtainStyledAttributes(attrs, R.styleable.pingleEditText)
         try {
             initView(typedArray)
@@ -28,16 +37,12 @@ class PingleEditText @JvmOverloads constructor(
     }
 
     private fun initView(typedArray: TypedArray) {
-        binding = EditTextPingleBinding.inflate(LayoutInflater.from(context), this, true)
         typedArray.apply {
             val title = getString(R.styleable.pingleEditText_title)
             binding.tvTitle.text = title
 
             val hint = getString(R.styleable.pingleEditText_hint)
             binding.etEditText.hint = hint
-
-            _etText = getString(R.styleable.pingleEditText_etText)
-            binding.etEditText.setText(_etText)
         }
     }
 }

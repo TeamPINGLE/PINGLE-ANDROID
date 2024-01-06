ec2721f5e76bc887e8a91851e8e777ae2ce6fac4
JIEUNI <jieundaeun@naver.com>
2024-01-07 02:26:03 +0900

    [mod] #37 ViewModel과 StateFlow를 활용한 로직으로 수정

--- app/src/main/java/org/sopt/pingle/presentation/ui/main/plan/PlanLocationViewModel.kt
+++ app/src/main/java/org/sopt/pingle/presentation/ui/main/plan/PlanLocationViewModel.kt
@@ -1,9 +1,37 @@
 package org.sopt.pingle.presentation.ui.main.plan
 
 import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import org.sopt.pingle.domain.model.PlanLocationEntity
 
 class PlanLocationViewModel : ViewModel() {
+    private val _planLocationList = MutableStateFlow<List<PlanLocationEntity>>(emptyList())
+    private val planLocationList get() = _planLocationList.asStateFlow()
+
+    private var oldPosition = -1
+    fun updatePlanLocationList(position: Int) {
+        if (oldPosition == -1 && oldPosition != position) {
+            setIsSelected(true, position)
+        } else if (oldPosition == position) {
+            setIsSelected(false, oldPosition)
+        } else {
+            setIsSelected(true, position)
+            setIsSelected(false, oldPosition)
+        }
+        oldPosition = position
+    }
+
+    fun checkIsNull(): Boolean {
+        return mockPlanLocationList.isEmpty()
+        // TODO return planLocationList.value.isEmpty()
+    }
+
+    private fun setIsSelected(value: Boolean, position: Int) {
+        mockPlanLocationList[position].isSelected.set(value)
+        // TODO 서버에서 받아올 리스트에 저장.. planLocationList.value[position].isSelected.set(value)
+    }
+
     val mockPlanLocationList = listOf<PlanLocationEntity>(
         PlanLocationEntity(
             location = "하얀집",

5b1856b1e1aea5b2adb3aee01b66bd2c5b65f9fe
JIEUNI <jieundaeun@naver.com>
2024-01-08 00:58:32 +0900

    [add] #22 카테고리 타입 UX라이팅 리소스 추가

--- app/src/main/java/org/sopt/pingle/presentation/type/CategoryType.kt
+++ app/src/main/java/org/sopt/pingle/presentation/type/CategoryType.kt
@@ -9,9 +9,9 @@ enum class CategoryType(
     @ColorRes val activatedOutLinedColor: Int,
     @ColorRes val backgroundChipColor: Int,
     @ColorRes val backgroundBadgeColor: Int,
-    @StringRes val categoryNameRes: Int
+    @StringRes val categoryNameRes: Int,
+    @StringRes val categoryDescriptionRes: Int,
     // TODO 해당 부분은 UX, icon 정해지면 추가하기
-    // @StringRes val categoryDescriptionRes: Int,
     // @DrawableRes val categoryIconRes: Int,
 ) {
     PLAY(
@@ -19,28 +19,32 @@ enum class CategoryType(
         activatedOutLinedColor = R.color.pingle_green,
         backgroundChipColor = R.color.chip_green,
         backgroundBadgeColor = R.color.badge_green,
-        categoryNameRes = R.string.category_play
+        categoryNameRes = R.string.category_play,
+        categoryDescriptionRes = R.string.category_play_detail,
     ),
     STUDY(
         textColor = R.color.pingle_orange,
         activatedOutLinedColor = R.color.pingle_orange,
         backgroundChipColor = R.color.chip_orange,
         backgroundBadgeColor = R.color.badge_orange,
-        categoryNameRes = R.string.category_study
+        categoryNameRes = R.string.category_study,
+        categoryDescriptionRes = R.string.category_study_detail,
     ),
     MULTI(
         textColor = R.color.pingle_yellow,
         activatedOutLinedColor = R.color.pingle_yellow,
         backgroundChipColor = R.color.chip_yellow,
         backgroundBadgeColor = R.color.badge_yellow,
-        categoryNameRes = R.string.category_multi
+        categoryNameRes = R.string.category_multi,
+        categoryDescriptionRes = R.string.category_multi_detail,
     ),
     OTHERS(
         textColor = R.color.g_01,
         activatedOutLinedColor = R.color.g_01,
         backgroundChipColor = R.color.g_10,
         backgroundBadgeColor = R.color.g_07,
-        categoryNameRes = R.string.category_others
+        categoryNameRes = R.string.category_others,
+        categoryDescriptionRes = R.string.category_others_detail,
     );
 
     companion object {

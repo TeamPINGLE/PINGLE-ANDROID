3ca38ed60c7e3c40d772f189bed5128188eea602
Dani43 <eldms0406@naver.com>
2024-01-07 16:32:12 +0900

    [feat] #44 번개 개최 뷰 - 제목, 오픈채팅방 구현

--- app/src/main/java/org/sopt/pingle/presentation/ui/main/plan/PlanOpenChattingFragment.kt
+++ app/src/main/java/org/sopt/pingle/presentation/ui/main/plan/PlanOpenChattingFragment.kt
@@ -2,12 +2,26 @@ package org.sopt.pingle.presentation.ui.main.plan
 
 import android.os.Bundle
 import android.view.View
+import androidx.fragment.app.activityViewModels
 import org.sopt.pingle.R
 import org.sopt.pingle.databinding.FragmentPlanOpenChattingBinding
 import org.sopt.pingle.util.base.BindingFragment
+import org.sopt.pingle.util.context.hideKeyboard
 
-class PlanOpenChattingFragment : BindingFragment<FragmentPlanOpenChattingBinding>(R.layout.fragment_plan_open_chatting) {
+class PlanOpenChattingFragment :
+    BindingFragment<FragmentPlanOpenChattingBinding>(R.layout.fragment_plan_open_chatting) {
+    private val planViewModel: PlanViewModel by activityViewModels()
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        binding.viewModel = planViewModel
+
+        addListeners()
+    }
+
+    private fun addListeners() {
+        binding.root.setOnClickListener {
+            requireActivity().hideKeyboard(it)
+        }
     }
 }

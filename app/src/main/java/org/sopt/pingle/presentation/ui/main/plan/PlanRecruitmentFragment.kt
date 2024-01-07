c8a84f0349696ad00c928099c76f5294ede196ab
JIEUNI <jieundaeun@naver.com>
2024-01-08 05:56:26 +0900

    [feat] #39 버튼 활성화 로직 추가

--- app/src/main/java/org/sopt/pingle/presentation/ui/main/plan/PlanRecruitmentFragment.kt
+++ app/src/main/java/org/sopt/pingle/presentation/ui/main/plan/PlanRecruitmentFragment.kt
@@ -1,24 +1,71 @@
 package org.sopt.pingle.presentation.ui.main.plan
 
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.flowWithLifecycle
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
 import org.sopt.pingle.R
 import org.sopt.pingle.databinding.FragmentPlanRecruitmentBinding
 import org.sopt.pingle.util.base.BindingFragment
+import org.sopt.pingle.util.component.CustomSnackbar
+import org.sopt.pingle.util.context.hideKeyboard
 
 class PlanRecruitmentFragment :
     BindingFragment<FragmentPlanRecruitmentBinding>(R.layout.fragment_plan_recruitment) {
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?,
-    ): View {
-        return super.onCreateView(inflater, container, savedInstanceState)
+
+    private val planViewModel: PlanViewModel by activityViewModels()
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.planViewModel = planViewModel
+        binding.lifecycleOwner = this
+
+        addListeners()
+        collectData()
     }
 
-    override fun onDestroyView() {
-        super.onDestroyView()
+    private fun addListeners() {
+        binding.root.setOnClickListener { requireActivity().hideKeyboard(it) }
+
+        binding.btnPlanRecruitmentPlus.setOnClickListener {
+            var i = planViewModel.selectedRecruitment.value?.toInt()
+            i = i!! + 1
+            planViewModel.selectedRecruitment.value = i.toString()
+        }
+
+        binding.btnPlanRecruitmentMinus.setOnClickListener {
+            var i = planViewModel.selectedRecruitment.value?.toInt()
+            i = i!! - 1
+            planViewModel.selectedRecruitment.value = i.toString()
+        }
+    }
+
+    private fun collectData() {
+        planViewModel.selectedRecruitment.flowWithLifecycle(lifecycle).onEach {
+            when (it.toString()) {
+                "99" -> {
+                    binding.btnPlanRecruitmentPlus.apply {
+                        isEnabled = false
+                    }
+                    CustomSnackbar.makeSnackbar(
+                        binding.layoutPlanRecruitment,
+                        getString(R.string.plan_recruitment_snackbar),
+                        126
+                    )
+                }
+
+                "1" -> {
+                    binding.btnPlanRecruitmentMinus.isEnabled = false
+                }
+
+                else -> {
+                    binding.btnPlanRecruitmentPlus.isEnabled = true
+                    binding.btnPlanRecruitmentMinus.isEnabled = true
+                }
+            }
+        }.launchIn(lifecycleScope)
     }
 }

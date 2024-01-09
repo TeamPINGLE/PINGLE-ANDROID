de2e080a4ecfc10dbcd1eedfb8015e46aca41724
jihyunniiii <jita1125@naver.com>
2024-01-10 04:22:23 +0900

    [feat] #78 참여자 페이지 구현 (공사장 뷰)

---
+++ app/src/main/java/org/sopt/pingle/presentation/ui/participant/ParticipantActivity.kt
@@ -0,0 +1,13 @@
+package org.sopt.pingle.presentation.ui.participant
+
+import android.os.Bundle
+import org.sopt.pingle.R
+import org.sopt.pingle.databinding.ActivityParticipantBinding
+import org.sopt.pingle.util.base.BindingActivity
+
+class ParticipantActivity :
+    BindingActivity<ActivityParticipantBinding>(R.layout.activity_participant) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}

17a8d765f351a8a47ffe9af8a4457b6a4a3532a3
JIEUNI <jieundaeun@naver.com>
2024-01-09 18:39:52 +0900

    [feat] #42 date 출력형태 가공 함수 추가

--- app/src/main/java/org/sopt/pingle/presentation/ui/main/plan/PlanSummaryConfirmationFragment.kt
+++ app/src/main/java/org/sopt/pingle/presentation/ui/main/plan/PlanSummaryConfirmationFragment.kt
@@ -27,12 +27,29 @@ class PlanSummaryConfirmationFragment :
                 tvPlanSummaryConfirmationName.text = viewModel.planTitle.value
                 tvPlanSummaryConfirmationOwnerName.text = "개최자"
                 tvPlanSummaryConfirmationCalenderDetail.text =
-                    viewModel.planDate.value + "\n" + makeTimeClean(viewModel.startTime.value) + " ~ " + makeTimeClean(viewModel.endTime.value)
+                    makeDateClean(viewModel.planDate.value) + "\n" + makeTimeClean(viewModel.startTime.value) + " ~ " + makeTimeClean(
+                    viewModel.endTime.value
+                )
                 tvPlanSummaryConfirmationMapDetail.text = viewModel.selectedLocation.value?.location
-                tvPlanSummaryConfirmationRecruitmentDetail.text = getString(R.string.plan_summary_confirmation_recruitment_number, viewModel.selectedRecruitment.value)
+                tvPlanSummaryConfirmationRecruitmentDetail.text = getString(
+                    R.string.plan_summary_confirmation_recruitment_number,
+                    viewModel.selectedRecruitment.value
+                )
             }
         }
     }
 
     private fun makeTimeClean(time: String): String = time.substring(0, 5)
+    private fun makeDateClean(date: String): String {
+        val year = date.substring(0, 4)
+        val month = date.substring(5, 7)
+        val day = date.substring(8, 10)
+        return year + YEAR + month + MONTH + day + DAY
+    }
+
+    companion object {
+        const val YEAR = "년 "
+        const val MONTH = "월 "
+        const val DAY = "일"
+    }
 }

3128158976587ce09e266cc1584f00177f1ffa8d
DoReMinWoo <kimy5878@naver.com>
2024-01-11 02:12:09 +0900

    [mod] #90 Header에 AccessToken넣기

--- app/src/main/java/org/sopt/pingle/data/interceptor/AuthInterceptor.kt
+++ app/src/main/java/org/sopt/pingle/data/interceptor/AuthInterceptor.kt
@@ -1,19 +1,22 @@
 package org.sopt.pingle.data.interceptor
 
-import javax.inject.Inject
 import okhttp3.Interceptor
 import okhttp3.Response
 import org.sopt.pingle.BuildConfig
+import org.sopt.pingle.data.datasource.local.PingleDataSource
+import javax.inject.Inject
 
-class AuthInterceptor @Inject constructor() : Interceptor {
+class AuthInterceptor @Inject constructor(
+    private val localStorage: PingleDataSource
+) : Interceptor {
     override fun intercept(chain: Interceptor.Chain): Response {
         val originalRequest = chain.request()
         val authRequest =
-            originalRequest.newBuilder().addHeader(ACCESS_TOKEN, BuildConfig.ACCESS_TOKEN).build()
+            originalRequest.newBuilder().addHeader(AUTHORIZATION, localStorage.accessToken).build()
         val response = chain.proceed(authRequest)
 
         when (response.code) {
-            401 -> {
+            CODE_TOKEN_EXPIRE -> {
                 // TODO 토큰 재발급 api 연동
             }
         }
@@ -21,6 +24,7 @@ class AuthInterceptor @Inject constructor() : Interceptor {
     }
 
     companion object {
-        const val ACCESS_TOKEN = "accessToken"
+        const val CODE_TOKEN_EXPIRE = 401
+        const val AUTHORIZATION = "Authorization"
     }
 }

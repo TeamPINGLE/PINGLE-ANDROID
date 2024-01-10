12bc668c2599a318000339962007fabb5cbdff3f
jihyunniiii <jita1125@naver.com>
2024-01-10 16:52:49 +0900

    [feat] #75 splash screen 구현

--- app/src/main/java/org/sopt/pingle/presentation/ui/splash/SplashActivity.kt
+++ app/src/main/java/org/sopt/pingle/presentation/ui/splash/SplashActivity.kt
@@ -2,12 +2,38 @@ package org.sopt.pingle.presentation.ui.splash
 
 import android.content.Intent
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import org.sopt.pingle.R
+import org.sopt.pingle.databinding.ActivitySplashBinding
 import org.sopt.pingle.presentation.ui.auth.AuthActivity
+import org.sopt.pingle.util.base.BindingActivity
 
-class SplashActivity : AppCompatActivity() {
+class SplashActivity : BindingActivity<ActivitySplashBinding>(R.layout.activity_splash) {
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
         super.onCreate(savedInstanceState)
-        startActivity(Intent(this, AuthActivity::class.java))
+        loadSplashScreen()
+    }
+
+    private fun loadSplashScreen() {
+        lifecycleScope.launch {
+            delay(SPLASH_SCREEN_DELAY_TIME)
+            navigateToOnBoarding()
+        }
+    }
+
+    private fun navigateToOnBoarding() {
+        // TODO 자동로그인 로직 추가
+        Intent(this@SplashActivity, AuthActivity::class.java).apply {
+            startActivity(this)
+            finish()
+        }
+    }
+
+    companion object {
+        const val SPLASH_SCREEN_DELAY_TIME = 1500L
     }
 }

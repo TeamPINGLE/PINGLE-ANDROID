4bd4e484a85aedf915f6d7ff61d1f58ddfdee4f3
jihyunniiii <jita1125@naver.com>
2024-01-11 12:21:23 +0900

    [chore] #96 함수 위치 수정

--- app/src/main/java/org/sopt/pingle/presentation/mapper/PinMapper.kt
+++ app/src/main/java/org/sopt/pingle/presentation/mapper/PinMapper.kt
@@ -4,8 +4,12 @@ import androidx.databinding.Observable
 import com.naver.maps.geometry.LatLng
 import com.naver.maps.map.overlay.Marker
 import org.sopt.pingle.domain.model.PinEntity
+import org.sopt.pingle.domain.model.PingleEntity
 import org.sopt.pingle.presentation.model.MarkerModel
 import org.sopt.pingle.presentation.type.CategoryType
+import java.time.LocalDate
+import java.time.LocalTime
+import java.time.format.DateTimeFormatter
 
 fun PinEntity.toMarkerModel(): MarkerModel {
     val markerModel = MarkerModel(
@@ -29,3 +33,18 @@ fun PinEntity.toMarkerModel(): MarkerModel {
 
     return markerModel
 }
+
+fun PingleEntity.isCompleted() = maxParticipants == curParticipants
+
+fun PingleEntity.convertToCalenderDetail(): String {
+    val localDate = LocalDate.parse(date, DateTimeFormatter.ISO_LOCAL_DATE)
+    val startTime = LocalTime.parse(startAt, DateTimeFormatter.ISO_LOCAL_TIME)
+    val endTime = LocalTime.parse(endAt, DateTimeFormatter.ISO_LOCAL_TIME)
+
+    return buildString {
+        append("${localDate.year}년 ${localDate.monthValue}월 ${localDate.dayOfMonth}일\n")
+        append("${startTime.format(DateTimeFormatter.ofPattern("HH:mm"))} ~ ")
+        append("${endTime.format(DateTimeFormatter.ofPattern("HH:mm"))}")
+    }
+}
+

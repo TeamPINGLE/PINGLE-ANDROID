c22bac373fc061dd3d45127c450227fed7f55ab1
Dani43 <eldms0406@naver.com>
2024-01-11 22:49:46 +0900

    [fet] #104 로그아웃 성공 시 쉐어드프리퍼런스 clear 및 로그인 페이지로 이동

--- app/src/main/java/org/sopt/pingle/presentation/ui/main/more/MoreFragment.kt
+++ app/src/main/java/org/sopt/pingle/presentation/ui/main/more/MoreFragment.kt
@@ -1,21 +1,37 @@
 package org.sopt.pingle.presentation.ui.main.more
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.flowWithLifecycle
+import androidx.lifecycle.lifecycleScope
 import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
 import org.sopt.pingle.BuildConfig
 import org.sopt.pingle.R
+import org.sopt.pingle.data.service.KakaoAuthService
 import org.sopt.pingle.databinding.FragmentMoreBinding
+import org.sopt.pingle.presentation.ui.auth.AuthActivity
 import org.sopt.pingle.util.base.BindingFragment
 import org.sopt.pingle.util.component.AllModalDialogFragment
+import org.sopt.pingle.util.view.UiState
+import timber.log.Timber
 
 @AndroidEntryPoint
 class MoreFragment : BindingFragment<FragmentMoreBinding>(R.layout.fragment_more) {
+    @Inject
+    lateinit var kakaoAuthService: KakaoAuthService
+    private val moreViewModel: MoreViewModel by viewModels()
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         initLayout()
         addListeners()
+        collectData()
     }
 
     private fun initLayout() {
@@ -31,6 +47,29 @@ class MoreFragment : BindingFragment<FragmentMoreBinding>(R.layout.fragment_more
         }
     }
 
+    private fun collectData() {
+        moreViewModel.logoutState.flowWithLifecycle(lifecycle).onEach { logoutState ->
+            when (logoutState) {
+                is UiState.Success -> {
+                    moveToSign()
+                }
+
+                is UiState.Error -> {
+                    Timber.d("로그아웃 실패")
+                }
+
+                else -> {}
+            }
+        }.launchIn(lifecycleScope)
+    }
+
+    private fun moveToSign() {
+        val intent = Intent(requireContext(), AuthActivity::class.java)
+        intent.flags =
+            Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
+        startActivity(intent)
+    }
+
     private fun showLogoutDialogFragment() {
         AllModalDialogFragment(
             title = getString(R.string.setting_logout_modal_title),
@@ -39,7 +78,7 @@ class MoreFragment : BindingFragment<FragmentMoreBinding>(R.layout.fragment_more
             textButtonText = getString(R.string.setting_logout_modal_btn_text),
             clickBtn = {},
             clickTextBtn = {
-                // TODO 로그아웃 서버통신
+                kakaoAuthService.logoutKakao(moreViewModel::logout)
             },
             onDialogClosed = {}
         ).show(parentFragmentManager, LOGOUT_MODAL)

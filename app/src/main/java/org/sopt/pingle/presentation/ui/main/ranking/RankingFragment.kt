cd5d2494aa363c54a2b6bcf6630a4342d31363e7
jihyunniiii <jita1125@naver.com>
2024-01-13 01:29:07 +0900

    [chore] #118 네이밍 변경 (commend -> ranking)

--- app/src/main/java/org/sopt/pingle/presentation/ui/main/ranking/RankingFragment.kt
+++ app/src/main/java/org/sopt/pingle/presentation/ui/main/ranking/RankingFragment.kt
@@ -1,12 +1,12 @@
-package org.sopt.pingle.presentation.ui.main.commend
+package org.sopt.pingle.presentation.ui.main.ranking
 
 import android.os.Bundle
 import android.view.View
 import org.sopt.pingle.R
-import org.sopt.pingle.databinding.FragmentCommendBinding
+import org.sopt.pingle.databinding.FragmentRankingBinding
 import org.sopt.pingle.util.base.BindingFragment
 
-class CommendFragment : BindingFragment<FragmentCommendBinding>(R.layout.fragment_commend) {
+class RankingFragment : BindingFragment<FragmentRankingBinding>(R.layout.fragment_ranking) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
     }

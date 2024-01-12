470c6461b3c84ce628912cedfed60c3943579a79
Dani43 <eldms0406@naver.com>
2024-01-13 01:23:24 +0900

    [feat] #121 토큰 재발급 구현

--- app/src/main/java/org/sopt/pingle/data/interceptor/AuthInterceptor.kt
+++ app/src/main/java/org/sopt/pingle/data/interceptor/AuthInterceptor.kt
@@ -1,13 +1,25 @@
 package org.sopt.pingle.data.interceptor
 
-import javax.inject.Inject
+import android.app.Application
+import android.content.Intent
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.serialization.json.Json
 import okhttp3.Interceptor
 import okhttp3.Request
+import okhttp3.RequestBody.Companion.toRequestBody
 import okhttp3.Response
+import org.sopt.pingle.BuildConfig
 import org.sopt.pingle.data.datasource.local.PingleLocalDataSource
+import org.sopt.pingle.data.model.remote.response.ResponseReissueDTO
+import org.sopt.pingle.util.base.BaseResponse
+import javax.inject.Inject
 
 class AuthInterceptor @Inject constructor(
-    private val localStorage: PingleLocalDataSource
+    private val json: Json,
+    private val localStorage: PingleLocalDataSource,
+    private val context: Application
 ) : Interceptor {
     override fun intercept(chain: Interceptor.Chain): Response {
         val originalRequest = chain.request()
@@ -17,7 +29,44 @@ class AuthInterceptor @Inject constructor(
 
         when (response.code) {
             CODE_TOKEN_EXPIRE -> {
-                // TODO 토큰 재발급 api 연동
+                response.close()
+                val refreshTokenRequest = originalRequest.newBuilder().get()
+                    .url("${BuildConfig.BASE_URL}/v1/auth/reissue")
+                    .post("".toRequestBody(null))
+                    .addHeader(AUTHORIZATION, localStorage.refreshToken)
+                    .build()
+                val refreshTokenResponse = chain.proceed(refreshTokenRequest)
+
+                if(refreshTokenResponse.isSuccessful){
+                    val responseRefresh =
+                        json.decodeFromString<BaseResponse<ResponseReissueDTO>>(
+                            refreshTokenResponse.body?.string()
+                                ?:throw  IllegalStateException("\"refreshTokenResponse is null $refreshTokenResponse\"")
+                        )
+
+                    with(localStorage){
+                        accessToken = responseRefresh.data.accessToken
+                        refreshToken = responseRefresh.data.refreshToken
+                    }
+
+                    refreshTokenResponse.close()
+
+                    val newRequest = originalRequest.newAuthBuilder()
+                    return chain.proceed(newRequest)
+                }else{
+                    with(context){
+                        CoroutineScope(Dispatchers.Main).launch {
+                            startActivity(
+                                Intent.makeRestartActivityTask(
+                                    packageManager.getLaunchIntentForPackage(packageName)?.component
+                                )
+                            )
+                            localStorage.clear()
+                        }
+                    }
+                }
+
+
             }
         }
         return response

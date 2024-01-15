d1c59cac044b6d7696ae606db74cf2e2230f7e0c
jihyunniiii <jita1125@naver.com>
2024-01-16 06:22:43 +0900

    [chore] #154 ExtensionUtils로 함수 이동

--- app/src/main/java/org/sopt/pingle/util/view/ViewExt.kt
+++ app/src/main/java/org/sopt/pingle/util/view/ViewExt.kt
@@ -20,5 +20,3 @@ class ItemDiffCallback<T : Any>(
     override fun areContentsTheSame(oldItem: T, newItem: T): Boolean =
         onContentsTheSame(oldItem, newItem)
 }
-
-fun Int.toPx(): Int = (this * Resources.getSystem().displayMetrics.density).toInt()

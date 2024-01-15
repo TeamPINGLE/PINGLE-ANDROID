a3dae7893ba9e9e5e2fe52b165d747244f62b595
jihyunniiii <jita1125@naver.com>
2024-01-16 06:34:46 +0900

    [feat] #154 날짜 포매팅 확장함수 적용

--- app/src/main/java/org/sopt/pingle/presentation/mapper/PingleMapper.kt
+++ app/src/main/java/org/sopt/pingle/presentation/mapper/PingleMapper.kt
@@ -8,15 +8,3 @@ import java.time.LocalTime
 import java.time.format.DateTimeFormatter
 
 fun PingleEntity.isCompleted() = maxParticipants == curParticipants
-
-fun PingleEntity.convertToCalenderDetail(): String = buildString {
-    append(convertToDateFormat(LocalDate.parse(date, DateTimeFormatter.ISO_LOCAL_DATE)))
-    append(NEW_LINE)
-    append(convertToTimeFormat(LocalTime.parse(startAt, DateTimeFormatter.ISO_LOCAL_TIME)))
-    append(TIME_SEPARATOR)
-    append(convertToTimeFormat(LocalTime.parse(startAt, DateTimeFormatter.ISO_LOCAL_TIME)))
-}
-
-const val NEW_LINE = "\n"
-const val TIME_SEPARATOR = " ~ "
-

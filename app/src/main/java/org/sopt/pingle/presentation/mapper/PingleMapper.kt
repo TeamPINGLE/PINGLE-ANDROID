4be05ff660f20f3755384599d1a1a16b5a0e95f6
jihyunniiii <jita1125@naver.com>
2024-01-16 06:23:19 +0900

    [chore] #154 날짜 포매팅 로직 함수화

--- app/src/main/java/org/sopt/pingle/presentation/mapper/PingleMapper.kt
+++ app/src/main/java/org/sopt/pingle/presentation/mapper/PingleMapper.kt
@@ -1,20 +1,22 @@
 package org.sopt.pingle.presentation.mapper
 
+import org.sopt.pingle.domain.model.PingleEntity
+import org.sopt.pingle.util.convertToDateFormat
+import org.sopt.pingle.util.convertToTimeFormat
 import java.time.LocalDate
 import java.time.LocalTime
 import java.time.format.DateTimeFormatter
-import org.sopt.pingle.domain.model.PingleEntity
 
 fun PingleEntity.isCompleted() = maxParticipants == curParticipants
 
-fun PingleEntity.convertToCalenderDetail(): String {
-    val localDate = LocalDate.parse(date, DateTimeFormatter.ISO_LOCAL_DATE)
-    val startTime = LocalTime.parse(startAt, DateTimeFormatter.ISO_LOCAL_TIME)
-    val endTime = LocalTime.parse(endAt, DateTimeFormatter.ISO_LOCAL_TIME)
-
-    return buildString {
-        append("${localDate.year}년 ${localDate.monthValue}월 ${localDate.dayOfMonth}일\n")
-        append("${startTime.format(DateTimeFormatter.ofPattern("HH:mm"))} ~ ")
-        append("${endTime.format(DateTimeFormatter.ofPattern("HH:mm"))}")
-    }
+fun PingleEntity.convertToCalenderDetail(): String = buildString {
+    append(convertToDateFormat(LocalDate.parse(date, DateTimeFormatter.ISO_LOCAL_DATE)))
+    append(NEW_LINE)
+    append(convertToTimeFormat(LocalTime.parse(startAt, DateTimeFormatter.ISO_LOCAL_TIME)))
+    append(TIME_SEPARATOR)
+    append(convertToTimeFormat(LocalTime.parse(startAt, DateTimeFormatter.ISO_LOCAL_TIME)))
 }
+
+const val NEW_LINE = "\n"
+const val TIME_SEPARATOR = " ~ "
+

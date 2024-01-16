32fb131f00b8bb1e8fc7e58d1bb3775c42933ae4
jihyunniiii <jita1125@naver.com>
2024-01-17 06:55:27 +0900

    [chore] #128 커스텀 스낵바 유형 분기 처리

--- app/src/main/java/org/sopt/pingle/util/component/PingleSnackbar.kt
+++ app/src/main/java/org/sopt/pingle/util/component/PingleSnackbar.kt
@@ -1,16 +1,24 @@
 package org.sopt.pingle.util.component
 
+import android.content.res.ColorStateList
 import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.FrameLayout
+import androidx.core.content.ContextCompat.getColor
 import com.google.android.material.snackbar.Snackbar
+import org.sopt.pingle.R
 import org.sopt.pingle.databinding.ViewSnackbarBinding
 import org.sopt.pingle.util.view.toPx
 
 object PingleSnackbar {
-    fun makeSnackbar(view: View, message: String, botMarin: Int) {
+    fun makeSnackbar(
+        view: View,
+        message: String,
+        bottomMarin: Int,
+        isWarning: Boolean = true
+    ) {
         val inflater = LayoutInflater.from(view.context)
         val binding = ViewSnackbarBinding.inflate(inflater, null, false)
 
@@ -25,9 +33,18 @@ object PingleSnackbar {
             width = ViewGroup.LayoutParams.MATCH_PARENT
             height = ViewGroup.LayoutParams.WRAP_CONTENT
             gravity = Gravity.CENTER_HORIZONTAL or Gravity.BOTTOM
-            bottomMargin = botMarin.toPx()
+            bottomMargin = bottomMarin.toPx()
             marginStart = 16.toPx()
             marginEnd = 16.toPx()
+        }
+
+        if (!isWarning) {
+            with(binding) {
+                layoutSnackbar.backgroundTintList =
+                    ColorStateList.valueOf(getColor(view.context, R.color.g_02))
+                ivSnackbarNotice.setImageResource(R.drawable.ic_all_notice_24)
+                tvSnackbar.setTextColor(getColor(view.context, R.color.black))
+            }
         }
 
         with(snackbarLayout) {

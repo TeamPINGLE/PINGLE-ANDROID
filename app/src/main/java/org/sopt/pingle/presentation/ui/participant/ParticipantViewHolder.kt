960997f78764173eb790bfae2d19e0bf082b69c7
JIEUNI <jieundaeun@naver.com>
2024-02-15 22:07:41 +0900

    [mod] #181 코드리뷰 반영

--- app/src/main/java/org/sopt/pingle/presentation/ui/participant/ParticipantViewHolder.kt
+++ app/src/main/java/org/sopt/pingle/presentation/ui/participant/ParticipantViewHolder.kt
@@ -6,19 +6,22 @@ import org.sopt.pingle.databinding.ItemParticipantBinding
 
 class ParticipantViewHolder(private val binding: ItemParticipantBinding) :
     RecyclerView.ViewHolder(binding.root) {
-    fun onBind(item: String) {
+    fun onBind(name: String) {
         with(binding) {
-            tvParticipantName.text = item
-            tvParticipantOwnerName.text = item
-
-            val isDefaultPosition = absoluteAdapterPosition == DEFAULT_POSITION
-            tvParticipantName.visibility = if (isDefaultPosition) View.INVISIBLE else View.VISIBLE
-            tvParticipantOwner.visibility = if (isDefaultPosition) View.VISIBLE else View.INVISIBLE
-            tvParticipantOwnerName.visibility = if (isDefaultPosition) View.VISIBLE else View.INVISIBLE
+            (absoluteAdapterPosition == ORGANIZER_POSITION).let { isOrganizerPosition ->
+                if (isOrganizerPosition) {
+                    tvParticipantOwnerName.text = name
+                } else {
+                    tvParticipantName.text = name
+                }
+                tvParticipantName.visibility = if (isOrganizerPosition) View.INVISIBLE else View.VISIBLE
+                tvParticipantOwner.visibility = if (isOrganizerPosition) View.VISIBLE else View.INVISIBLE
+                tvParticipantOwnerName.visibility = if (isOrganizerPosition) View.VISIBLE else View.INVISIBLE
+            }
         }
     }
 
     companion object {
-        const val DEFAULT_POSITION = 0
+        const val ORGANIZER_POSITION = 0
     }
 }

6b4e4b779a9578a16533040acc3648d029819230
jihyunniiii <jita1125@naver.com>
2024-02-16 19:41:19 +0900

    [feat] #186 HomeFragment 뷰 전환 로직 구현

--- app/src/main/java/org/sopt/pingle/presentation/ui/main/home/HomeFragment.kt
+++ app/src/main/java/org/sopt/pingle/presentation/ui/main/home/HomeFragment.kt
@@ -2,13 +2,80 @@ package org.sopt.pingle.presentation.ui.main.home
 
 import android.os.Bundle
 import android.view.View
+import androidx.fragment.app.Fragment
+import androidx.viewpager2.widget.ViewPager2
 import org.sopt.pingle.R
 import org.sopt.pingle.databinding.FragmentHomeBinding
+import org.sopt.pingle.presentation.type.CategoryType
+import org.sopt.pingle.presentation.ui.main.home.mainlist.MainListFragment
+import org.sopt.pingle.presentation.ui.main.home.map.MapFragment
 import org.sopt.pingle.util.base.BindingFragment
+import org.sopt.pingle.util.view.PingleFragmentStateAdapter
 
 class HomeFragment : BindingFragment<FragmentHomeBinding>(R.layout.fragment_home) {
+    private lateinit var fragmentList: ArrayList<Fragment>
+    private lateinit var fragmentStateAdapter: PingleFragmentStateAdapter
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        initLayout()
+        addListeners()
+        setFragmentStateAdapter()
     }
 
+    private fun initLayout() {
+        with(binding) {
+            chipHomeCategoryPlay.setChipCategoryType(CategoryType.PLAY)
+            chipHomeCategoryStudy.setChipCategoryType(CategoryType.STUDY)
+            chipHomeCategoryMulti.setChipCategoryType(CategoryType.MULTI)
+            chipHomeCategoryOthers.setChipCategoryType(CategoryType.OTHERS)
+        }
+    }
+
+    private fun addListeners() {
+        with(binding) {
+            fabHomeChange.setOnClickListener {
+                vpHome.setCurrentItem(
+                    when (vpHome.currentItem) {
+                        HOME_INDEX -> MAIN_LIST_INDEX
+                        else -> HOME_INDEX
+                    }, false
+                )
+            }
+        }
+    }
+
+    private fun setFragmentStateAdapter() {
+        fragmentList = ArrayList()
+        fragmentList.apply {
+            add(MapFragment())
+            add(MainListFragment())
+        }
+
+        fragmentStateAdapter = PingleFragmentStateAdapter(fragmentList, requireActivity())
+
+        with(binding.vpHome) {
+            adapter = fragmentStateAdapter
+            isUserInputEnabled = false
+            registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback(){
+                override fun onPageSelected(position: Int) {
+                    super.onPageSelected(position)
+                    with(binding) {
+                        fabHomeChange.setImageResource(
+                            when(vpHome.currentItem) {
+                                HOME_INDEX -> R.drawable.ic_map_list_24
+                                else -> R.drawable.ic_map_map_24
+                            }
+                        )
+                    }
+                }
+            })
+        }
+    }
+
+    companion object {
+        const val HOME_INDEX = 0
+        const val MAIN_LIST_INDEX = 1
+    }
 }

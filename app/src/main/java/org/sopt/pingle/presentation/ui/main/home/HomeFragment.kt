5bb22dce2f861c7c6cdf404e83c8720131844453
jihyunniiii <jita1125@naver.com>
2024-02-16 09:09:26 +0900

    [feat] #186 HomeFragment 생성

---
+++ app/src/main/java/org/sopt/pingle/presentation/ui/main/home/HomeFragment.kt
@@ -0,0 +1,14 @@
+package org.sopt.pingle.presentation.ui.main.home
+
+import android.os.Bundle
+import android.view.View
+import org.sopt.pingle.R
+import org.sopt.pingle.databinding.FragmentHomeBinding
+import org.sopt.pingle.util.base.BindingFragment
+
+class HomeFragment : BindingFragment<FragmentHomeBinding>(R.layout.fragment_home) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+}

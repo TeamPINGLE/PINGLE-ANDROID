8b48fa2bc08e4e5ee4c0b8260b15835a1bf111aa
MIN WOO KIM <kimy5878@naver.com>
2024-02-18 19:17:45 +0900

    [feat] #178 fragemnt_new_group_create View 구현

--- app/src/main/java/org/sopt/pingle/presentation/ui/newgroup/NewGroupCreateFragment.kt
+++ app/src/main/java/org/sopt/pingle/presentation/ui/newgroup/NewGroupCreateFragment.kt
@@ -3,11 +3,11 @@ package org.sopt.pingle.presentation.ui.newgroup
 import android.os.Bundle
 import android.view.View
 import org.sopt.pingle.R
-import org.sopt.pingle.databinding.FragmentNewGroupInfoBinding
+import org.sopt.pingle.databinding.FragmentNewGroupCreateBinding
 import org.sopt.pingle.util.base.BindingFragment
 
-class NewGroupInfoFragment :
-    BindingFragment<FragmentNewGroupInfoBinding>(R.layout.fragment_new_group_info) {
+class NewGroupCreateFragment :
+    BindingFragment<FragmentNewGroupCreateBinding>(R.layout.fragment_new_group_create) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
     }

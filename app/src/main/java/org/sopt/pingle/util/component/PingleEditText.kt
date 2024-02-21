a2e040979edac20717f46b2dedf830c870175f29
김민우 <kimy5878@naver.com>
2024-02-22 02:20:19 +0900

    [mod] #178 icon/btn visibility 적용

--- app/src/main/java/org/sopt/pingle/util/component/PingleEditText.kt
+++ app/src/main/java/org/sopt/pingle/util/component/PingleEditText.kt
@@ -6,6 +6,7 @@ import android.content.res.TypedArray
 import android.text.InputFilter
 import android.util.AttributeSet
 import android.view.LayoutInflater
+import android.view.View
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.databinding.DataBindingUtil
 import org.sopt.pingle.R
@@ -19,7 +20,6 @@ class PingleEditText @JvmOverloads constructor(
 ) : ConstraintLayout(context, attrs, defStyleAttr) {
     private var binding: EditTextPingleBinding
     val editText get() = binding.etEditText
-    val button get() = binding.btnCheck
 
     init {
         binding = DataBindingUtil.inflate(
@@ -49,6 +49,19 @@ class PingleEditText @JvmOverloads constructor(
             if (maxLength > 0) {
                 binding.etEditText.filters = arrayOf(InputFilter.LengthFilter(maxLength))
             }
+
+            val checkVisibilityValue =
+                getInt(R.styleable.pingleEditText_check_visibility, View.GONE)
+            val copyVisibilityValue = getInt(R.styleable.pingleEditText_copy_visibility, View.GONE)
+            binding.btnEditCheck.visibility = visibility(checkVisibilityValue)
+            binding.ivEditCopy.visibility = visibility(copyVisibilityValue)
         }
+    }
+
+    private fun visibility(visibilityValue: Int) = when (visibilityValue) {
+        1 -> View.VISIBLE
+        2 -> View.INVISIBLE
+        3 -> View.GONE
+        else -> View.GONE
     }
 }

b0f2b8fd88d7f605d7dfba7d1f2a9ad653020c38
JIEUNI <jieundaeun@naver.com>
2024-02-21 19:32:50 +0900

    [chore] #176 코드리뷰 반영

--- app/src/main/java/org/sopt/pingle/presentation/ui/onboarding/OnboardingExplanationTitleViewHolder.kt
+++ app/src/main/java/org/sopt/pingle/presentation/ui/onboarding/OnboardingExplanationTitleViewHolder.kt
@@ -14,7 +14,25 @@ class OnboardingExplanationTitleViewHolder(
 ) :
     RecyclerView.ViewHolder(binding.root) {
     fun onBind() {
-        binding.tvOnboardingExplanationTitle.text = SpannableString(
+        val titleText = getString(itemView.context, R.string.on_boarding_explanation_logo_title)
+        val pingleIndex = titleText.indexOf(PINGLE)
+
+        if (pingleIndex != INVALID_INDEX) {
+            binding.tvOnboardingExplanationTitle.text = SpannableString(titleText).apply {
+                setSpan(
+                    ForegroundColorSpan(
+                        ContextCompat.getColor(itemView.context, R.color.pingle_green)
+                    ),
+                    pingleIndex,
+                    pingleIndex + PINGLE.length,
+                    Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+                )
+            }
+        } else {
+            binding.tvOnboardingExplanationTitle.text = titleText
+        }
+
+        /*binding.tvOnboardingExplanationTitle.text = SpannableString(
             getString(itemView.context, R.string.on_boarding_explanation_logo_title)
         ).apply {
             setSpan(
@@ -25,11 +43,13 @@ class OnboardingExplanationTitleViewHolder(
                 PINGLE_END,
                 Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
             )
-        }
+        }*/
     }
 
     companion object {
         private const val PINGLE_START = 7
         private const val PINGLE_END = 14
+        private const val PINGLE = "PINGLE"
+        private const val INVALID_INDEX = -1
     }
 }

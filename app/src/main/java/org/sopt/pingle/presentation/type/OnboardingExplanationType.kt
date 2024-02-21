ea476ffdf4baf383739fe2901f11c89ffc694637
JIEUNI <jieundaeun@naver.com>
2024-02-21 18:03:13 +0900

    [chore] #176 온보딩 추가설명 뷰 : enum class에 FIRST page에 대한 주석 추가

--- app/src/main/java/org/sopt/pingle/presentation/type/OnboardingExplanationType.kt
+++ app/src/main/java/org/sopt/pingle/presentation/type/OnboardingExplanationType.kt
@@ -5,16 +5,17 @@ import androidx.annotation.StringRes
 import org.sopt.pingle.R
 
 enum class OnboardingExplanationType(@StringRes val description: Int, @DrawableRes val image: Int) {
+    // For the FIRST page, check layout > item_onboarding_expansion_tile.xml
     SECOND(
         R.string.on_boarding_explanation_group_description,
-        R.drawable.img_onboarding_explanation_group
+        R.drawable.img_onboarding_explanation_group,
     ),
     THIRD(
         R.string.on_boarding_explanation_pingle_description,
-        R.drawable.img_onboarding_explanation_pingle
+        R.drawable.img_onboarding_explanation_pingle,
     ),
     FOURTH(
         R.string.on_boarding_explanation_category_description,
-        R.drawable.img_onboarding_explanation_category
-    )
+        R.drawable.img_onboarding_explanation_category,
+        ),
 }

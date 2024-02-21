2c4396227af4fd77492d42edc93f7a56f45c6848
JIEUNI <jieundaeun@naver.com>
2024-02-21 17:33:24 +0900

    [feat] #176 온보딩 추가설명 뷰 : 안드로이드 기본 이전 버튼 관리

--- app/src/main/java/org/sopt/pingle/presentation/ui/onboarding/OnboardingExplanationActivity.kt
+++ app/src/main/java/org/sopt/pingle/presentation/ui/onboarding/OnboardingExplanationActivity.kt
@@ -2,20 +2,31 @@ package org.sopt.pingle.presentation.ui.onboarding
 
 import android.content.Intent
 import android.os.Bundle
+import androidx.activity.OnBackPressedCallback
 import com.google.android.material.tabs.TabLayoutMediator
 import org.sopt.pingle.R
 import org.sopt.pingle.databinding.ActivityOnboardingExplanationBinding
+import org.sopt.pingle.presentation.type.SnackbarType
 import org.sopt.pingle.presentation.ui.auth.AuthActivity
+import org.sopt.pingle.util.activity.FINISH_INTERVAL_TIME
+import org.sopt.pingle.util.activity.INIT_BACK_PRESSED_TIME
+import org.sopt.pingle.util.activity.SNACKBAR_BOTTOM_MARGIN
 import org.sopt.pingle.util.base.BindingActivity
+import org.sopt.pingle.util.component.PingleSnackbar
+import org.sopt.pingle.util.context.stringOf
 
 class OnboardingExplanationActivity :
     BindingActivity<ActivityOnboardingExplanationBinding>(R.layout.activity_onboarding_explanation) {
+
+    private lateinit var onBackPressed: OnBackPressedCallback
+    private var backPressedTime = INIT_BACK_PRESSED_TIME
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         initLayout()
         addListeners()
+        onBackPressedBtn()
     }
 
     private fun initLayout() {
@@ -43,10 +54,38 @@ class OnboardingExplanationActivity :
         }
     }
 
+    private fun onBackPressedBtn() {
+        onBackPressed = object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                navigateToPreviousPageOrExit()
+            }
+        }
+        onBackPressedDispatcher.addCallback(this, onBackPressed)
+    }
+
     private fun navigateToAuth() {
         Intent(this, AuthActivity::class.java).apply {
             startActivity(this)
-            finish()
+        }
+    }
+
+    private fun navigateToPreviousPageOrExit() {
+        with(binding) {
+            if (vpOnboardingExplanation.currentItem == OnboardingExplanationAdapter.DEFAULT_POSITION) {
+                if (System.currentTimeMillis() - backPressedTime <= FINISH_INTERVAL_TIME) {
+                    finish()
+                } else {
+                    backPressedTime = System.currentTimeMillis()
+                    PingleSnackbar.makeSnackbar(
+                        view = binding.root,
+                        message = stringOf(R.string.all_on_back_pressed_snackbar),
+                        bottomMarin = SNACKBAR_BOTTOM_MARGIN,
+                        snackbarType = SnackbarType.GUIDE,
+                    )
+                }
+            } else {
+                vpOnboardingExplanation.currentItem--
+            }
         }
     }
 }

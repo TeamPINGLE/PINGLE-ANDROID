d2af8d38aa8743c21b946e7d9fc1f7612e80fdd1
jihyunniiii <jita1125@naver.com>
2024-02-26 01:54:18 +0900

    [feat] #202 SearchView 텍스트 변경사항 반영 (Spannable)

--- app/src/main/java/org/sopt/pingle/presentation/ui/search/SearchActivity.kt
+++ app/src/main/java/org/sopt/pingle/presentation/ui/search/SearchActivity.kt
@@ -2,6 +2,9 @@ package org.sopt.pingle.presentation.ui.search
 
 import android.content.Intent
 import android.os.Bundle
+import android.text.Spannable
+import android.text.SpannableString
+import android.text.style.ForegroundColorSpan
 import android.view.KeyEvent
 import android.view.View
 import org.sopt.pingle.R
@@ -10,6 +13,7 @@ import org.sopt.pingle.presentation.model.SearchModel
 import org.sopt.pingle.presentation.ui.main.home.HomeFragment.Companion.SEARCH_MODEL
 import org.sopt.pingle.util.Intent.getCompatibleParcelableExtra
 import org.sopt.pingle.util.base.BindingActivity
+import org.sopt.pingle.util.context.colorOf
 import org.sopt.pingle.util.context.hideKeyboard
 import org.sopt.pingle.util.context.stringOf
 
@@ -28,7 +32,23 @@ class SearchActivity : BindingActivity<ActivitySearchBinding>(R.layout.activity_
             with(searchModel.homeViewType) {
                 binding.pingleSearchSearch.binding.etSearchPingleEditText.hint =
                     stringOf(searchHintRes)
-                binding.tvSearchSearchDescription.text = stringOf(searchDescriptionRes)
+                // TODO jihyun 추후 Spannable 관련 확장함수 생성
+                binding.tvSearchSearchDescription.text = getString(
+                    R.string.search_description,
+                    stringOf(searchDescriptionRes)
+                ).let { searchDescription ->
+                    SpannableString(searchDescription).apply {
+                        searchDescription.indexOf(getString(searchDescriptionRes))
+                            .takeIf { it != INVALID_INDEX }?.let { index ->
+                                setSpan(
+                                    ForegroundColorSpan(colorOf(R.color.pingle_green)),
+                                    index,
+                                    index + getString(searchDescriptionRes).length,
+                                    Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+                                )
+                            }
+                    }
+                }
             }
 
             binding.pingleSearchSearch.binding.etSearchPingleEditText.run {
@@ -67,8 +87,7 @@ class SearchActivity : BindingActivity<ActivitySearchBinding>(R.layout.activity_
     }
 
     companion object {
-        private const val MAP = "지도"
-        private const val MAIN_LIST = "리스트"
+        private const val INVALID_INDEX = -1
         const val SEARCH_WORD = "searchWord"
     }
 }

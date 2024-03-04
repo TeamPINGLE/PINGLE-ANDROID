f5cf20feb5032703eac3a346514745461b9d9db4
jihyunniiii <jita1125@naver.com>
2024-03-04 15:50:22 +0900

    [feat] #218 AmplitudeUtils 추가

---
+++ app/src/main/java/org/sopt/pingle/util/AmplitudeUtils.kt
@@ -0,0 +1,33 @@
+package org.sopt.pingle.util
+
+import android.content.Context
+import com.amplitude.android.Amplitude
+import com.amplitude.android.Configuration
+import org.sopt.pingle.BuildConfig
+
+object AmplitudeUtils {
+    private lateinit var amplitude: Amplitude
+    fun initAmplitude(context: Context) {
+        amplitude = Amplitude(
+            Configuration(
+                apiKey = BuildConfig.AMPLITUDE_API_KEY,
+                context = context
+            )
+        )
+    }
+
+    fun trackEvent(eventName: String) {
+        amplitude.track(eventType = eventName)
+    }
+
+    fun <T> trackEventWithProperty(eventName: String, propertyName: String, propertyValue: T) {
+        amplitude.track(
+            eventType = eventName,
+            eventProperties = mapOf(propertyName to propertyValue)
+        )
+    }
+
+    fun trackEventWithProperties(eventName: String, properties: Map<String, Any>) {
+        amplitude.track(eventType = eventName, eventProperties = properties)
+    }
+}

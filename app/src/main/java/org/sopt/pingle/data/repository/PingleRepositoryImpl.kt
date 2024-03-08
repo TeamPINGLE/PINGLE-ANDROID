0fca64f8150c375bffaa96db55a3e647659aee84
jihyunniiii <jita1125@naver.com>
2024-03-08 12:59:54 +0900

    [refactor] #222 핑글 참여, 삭제 Flow -> Result

--- app/src/main/java/org/sopt/pingle/data/repository/PingleRepositoryImpl.kt
+++ app/src/main/java/org/sopt/pingle/data/repository/PingleRepositoryImpl.kt
@@ -1,28 +1,24 @@
 package org.sopt.pingle.data.repository
 
-import javax.inject.Inject
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 import org.sopt.pingle.data.datasource.remote.PingleRemoteDataSource
 import org.sopt.pingle.domain.model.MyPingleEntity
 import org.sopt.pingle.domain.repository.PingleRepository
+import javax.inject.Inject
 
 class PingleRepositoryImpl @Inject constructor(
     private val pingleRemoteDataSource: PingleRemoteDataSource
 ) : PingleRepository {
-    override suspend fun postPingleJoin(meetingId: Long): Flow<Unit?> = flow {
-        val result = runCatching {
+    override suspend fun postPingleJoin(meetingId: Long): Result<Unit?> =
+        runCatching {
             pingleRemoteDataSource.postPingleJoin(meetingId = meetingId).data
         }
-        emit(result.getOrThrow())
-    }
 
-    override suspend fun deletePingleCancel(meetingId: Long): Flow<Unit?> = flow {
-        val result = runCatching {
+    override suspend fun deletePingleCancel(meetingId: Long): Result<Unit?> =
+        runCatching {
             pingleRemoteDataSource.deletePingleCancel(meetingId = meetingId).data
         }
-        emit(result.getOrThrow())
-    }
 
     override suspend fun getMyPingleList(
         teamId: Int,

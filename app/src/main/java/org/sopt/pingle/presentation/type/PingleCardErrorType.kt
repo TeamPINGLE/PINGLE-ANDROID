c791c6c5c785bcd4fd30e97d0720ebe0c8ea656a
jihyunniiii <jita1125@naver.com>
2024-03-08 14:40:32 +0900

    [chore] #222 PingleCardErrorType 수정

--- app/src/main/java/org/sopt/pingle/presentation/type/PingleCardErrorType.kt
+++ app/src/main/java/org/sopt/pingle/presentation/type/PingleCardErrorType.kt
@@ -5,22 +5,14 @@ import org.sopt.pingle.R
 
 enum class PingleCardErrorType(
     val code: Int,
-    @StringRes val message: Int,
     @StringRes val snackbarStringRes: Int
 ) {
-    DELETED_PINGLE_AT_JOIN(
+    DELETED(
         code = 404,
-        message = R.string.pingle_card_error_deleted_pingle_at_join_message,
-        snackbarStringRes = R.string.pingle_card_error_delete_snackbar
+        snackbarStringRes = R.string.pingle_card_error_deleted_snackbar
     ),
-    COMPLETED_PINGLE_AT_JOIN(
+    COMPLETED(
         code = 409,
-        message = R.string.pingle_card_error_completed_pingle_at_join_message,
         snackbarStringRes = R.string.pingle_card_error_completed_snackbar
-    ),
-    DELETED_PINGLE_AT_CANCEL(
-        code = 404,
-        message = R.string.pingle_card_error_deleted_pingle_at_cancel_message,
-        snackbarStringRes = R.string.pingle_card_error_delete_snackbar
     )
 }

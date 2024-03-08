e8c42e925322660dbb440e0c1c54cb8d683deee5
jihyunniiii <jita1125@naver.com>
2024-03-08 18:04:45 +0900

    [chore] #222 error message 로직 수정 (MyPingleViewModel)

--- app/src/main/java/org/sopt/pingle/presentation/ui/main/mypingle/MyPingleViewModel.kt
+++ app/src/main/java/org/sopt/pingle/presentation/ui/main/mypingle/MyPingleViewModel.kt
@@ -3,20 +3,22 @@ package org.sopt.pingle.presentation.ui.main.mypingle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
-import javax.inject.Inject
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
+import kotlinx.serialization.json.Json
 import org.sopt.pingle.data.datasource.local.PingleLocalDataSource
 import org.sopt.pingle.domain.model.MyPingleEntity
 import org.sopt.pingle.domain.usecase.DeletePingleCancelUseCase
 import org.sopt.pingle.domain.usecase.DeletePingleDeleteUseCase
 import org.sopt.pingle.domain.usecase.GetMyPingleListUseCase
 import org.sopt.pingle.presentation.type.MyPingleType
+import org.sopt.pingle.util.base.NullableBaseResponse
 import org.sopt.pingle.util.view.UiState
 import retrofit2.HttpException
+import javax.inject.Inject
 
 @HiltViewModel
 class MyPingleViewModel @Inject constructor(
@@ -114,8 +116,10 @@ class MyPingleViewModel @Inject constructor(
             }.onFailure { exception: Throwable ->
                 _myPingleState.emit(
                     UiState.Error(
-                        message = (exception as? HttpException)?.response()?.message()
-                            ?: exception.message,
+                        message = if (exception is HttpException) exception.response()?.errorBody()
+                            ?.byteString()?.utf8()?.let { errorBodyJson ->
+                                Json.decodeFromString<NullableBaseResponse<Unit>>(errorBodyJson).message
+                            } else exception.message,
                         code = (exception as? HttpException)?.response()?.code()
                     )
                 )

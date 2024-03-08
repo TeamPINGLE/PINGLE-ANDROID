943909a155e8899dd499f6f9a00ef4969dca9849
jihyunniiii <jita1125@naver.com>
2024-03-08 12:50:41 +0900

    [chore] #222 Error 분기처리를 위한 UiState 수정

--- app/src/main/java/org/sopt/pingle/util/view/UiState.kt
+++ app/src/main/java/org/sopt/pingle/util/view/UiState.kt
@@ -4,5 +4,5 @@ sealed class UiState<out T> {
     object Loading : UiState<Nothing>()
     object Empty : UiState<Nothing>()
     data class Success<T>(val data: T) : UiState<T>()
-    data class Error(val message: String?) : UiState<Nothing>()
+    data class Error(val message: String?, val code: Int? = null) : UiState<Nothing>()
 }

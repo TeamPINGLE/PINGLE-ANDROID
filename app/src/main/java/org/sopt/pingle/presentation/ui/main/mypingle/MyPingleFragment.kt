376374a41624777180df1b17a4f1a87431577783
김민우 <kimy5878@naver.com>
2024-03-08 22:08:40 +0900

    [feat] #234 앰플리튜드 트래킹 이벤트 코드 심기
    scroll_soonpingle, scroll_donepingle 제외!

--- app/src/main/java/org/sopt/pingle/presentation/ui/main/mypingle/MyPingleFragment.kt
+++ app/src/main/java/org/sopt/pingle/presentation/ui/main/mypingle/MyPingleFragment.kt
@@ -20,6 +20,7 @@ import org.sopt.pingle.presentation.type.SnackbarType
 import org.sopt.pingle.presentation.ui.main.home.HomeFragment
 import org.sopt.pingle.presentation.ui.main.home.map.MapFragment.Companion.MEETING_ID
 import org.sopt.pingle.presentation.ui.participant.ParticipantActivity
+import org.sopt.pingle.util.AmplitudeUtils
 import org.sopt.pingle.util.base.BindingFragment
 import org.sopt.pingle.util.component.AllModalDialogFragment
 import org.sopt.pingle.util.component.PingleSnackbar
@@ -59,8 +60,15 @@ class MyPingleFragment : BindingFragment<FragmentMyPingleBinding>(R.layout.fragm
         binding.tlMyPingle.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
             override fun onTabSelected(tab: TabLayout.Tab) {
                 when (tab.position) {
-                    MyPingleType.SOON.tabPosition -> viewModel.setMyPingleType(MyPingleType.SOON)
-                    MyPingleType.DONE.tabPosition -> viewModel.setMyPingleType(MyPingleType.DONE)
+                    MyPingleType.SOON.tabPosition -> {
+                        viewModel.setMyPingleType(MyPingleType.SOON)
+                        AmplitudeUtils.trackEvent(CLICK_SOONPINGLE)
+                    }
+
+                    MyPingleType.DONE.tabPosition -> {
+                        viewModel.setMyPingleType(MyPingleType.DONE)
+                        AmplitudeUtils.trackEvent(CLICK_DONEPINGLE)
+                    }
                 }
             }
 
@@ -126,7 +134,7 @@ class MyPingleFragment : BindingFragment<FragmentMyPingleBinding>(R.layout.fragm
             buttonText = stringOf(R.string.cancel_modal_button_text),
             textButtonText = stringOf(R.string.cancel_modal_text_button_text),
             clickBtn = { viewModel.deletePingleCancel(meetingId = myPingleEntity.id.toLong()) },
-            clickTextBtn = { }
+            clickTextBtn = { AmplitudeUtils.trackEvent(CLICK_SOONPINGLE_MORE_CANCEL_BACK) }
         ).show(childFragmentManager, MY_PINGLE_CANCEL_MODAL)
     }
 
@@ -161,5 +169,11 @@ class MyPingleFragment : BindingFragment<FragmentMyPingleBinding>(R.layout.fragm
         private const val DELETED_PINGLE_MESSAGE = "존재하지 않는 유저미팅입니다."
         const val MY_PINGLE_CANCEL_MODAL = "MyPingleCancelModal"
         const val MY_PINGLE_DELETE_MODAL = "MyPingleDeleteModal"
+
+        const val CLICK_SOONPINGLE = "click_soonpingle"
+        const val SCROLL_SOONPINGLE = "scroll_soonpingle"
+        const val CLICK_SOONPINGLE_MORE_CANCEL_BACK = "click_soonpingle_more_cancel_back"
+        const val CLICK_DONEPINGLE = "click_donepingle"
+        const val SCROLL_DONEPINGLE = "scroll_donepingle"
     }
 }

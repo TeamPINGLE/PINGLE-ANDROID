ad1214019bf73f08475573c58e2173c715e80f92
김민우 <kimy5878@naver.com>
2024-03-10 19:53:47 +0900

    [fix] #242 소셜로그인 후 앱 재실행시 입장방식선택화면으로 이동

--- app/src/main/java/org/sopt/pingle/presentation/ui/splash/SplashActivity.kt
+++ app/src/main/java/org/sopt/pingle/presentation/ui/splash/SplashActivity.kt
@@ -2,17 +2,19 @@ package org.sopt.pingle.presentation.ui.splash
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.lifecycle.lifecycleScope
 import dagger.hilt.android.AndroidEntryPoint
-import javax.inject.Inject
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import org.sopt.pingle.R
 import org.sopt.pingle.data.datasource.local.PingleLocalDataSource
 import org.sopt.pingle.databinding.ActivitySplashBinding
+import org.sopt.pingle.presentation.ui.onboarding.onboarding.OnboardingActivity
 import org.sopt.pingle.presentation.ui.onboarding.onboardingexplanation.OnboardingExplanationActivity
 import org.sopt.pingle.util.base.BindingActivity
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class SplashActivity : BindingActivity<ActivitySplashBinding>(R.layout.activity_splash) {
@@ -27,12 +29,23 @@ class SplashActivity : BindingActivity<ActivitySplashBinding>(R.layout.activity_
     private fun loadSplashScreen() {
         lifecycleScope.launch {
             delay(SPLASH_SCREEN_DELAY_TIME)
-            navigateToAuth()
+            if (localStorage.accessToken != "") {
+                navigateToOnboarding()
+            } else {
+                navigateToOnboardingExplanation()
+            }
         }
     }
 
-    private fun navigateToAuth() {
+    private fun navigateToOnboardingExplanation() {
         Intent(this, OnboardingExplanationActivity::class.java).apply {
+            startActivity(this)
+        }
+        finish()
+    }
+
+    private fun navigateToOnboarding() {
+        Intent(this, OnboardingActivity::class.java).apply {
             startActivity(this)
         }
         finish()

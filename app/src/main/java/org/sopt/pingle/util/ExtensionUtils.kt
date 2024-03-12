68b04c43613b0722da15bcdd7c07f63d2d536873
JIEUNI <jieundaeun@naver.com>
2024-03-12 10:11:08 +0900

    [feat] #244 단체명 13자 이상일 시 12까지만 노출시키고 ... 처리하는 확장함수 구현

--- app/src/main/java/org/sopt/pingle/util/ExtensionUtils.kt
+++ app/src/main/java/org/sopt/pingle/util/ExtensionUtils.kt
@@ -3,3 +3,13 @@ package org.sopt.pingle.util
 import android.content.res.Resources
 
 fun Int.toPx(): Int = (this * Resources.getSystem().displayMetrics.density).toInt()
+
+fun String.makeEllipsisGroupName(): String = when {
+    this.length > GROUP_NAME_MAX_LENGTH -> this.substring(SUBSTRING_START_INDEX, SUBSTRING_END_INDEX) + ELLIPSIS
+    else -> this
+}
+
+private const val SUBSTRING_START_INDEX = 0
+private const val SUBSTRING_END_INDEX = 12
+private const val GROUP_NAME_MAX_LENGTH = 13
+private const val ELLIPSIS = "..."
